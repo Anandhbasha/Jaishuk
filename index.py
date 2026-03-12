ed195b6c6893d0cbf724a87030b536a68612b6f0
Anandhbasha <anadhakm@gmail.com>
2026-03-12 20:53:40 +0530

    basics Python

---
+++ index.py
@@ -0,0 +1,53 @@
+'''print("Hello World",end=" ")
+print("Welcome to Python")
+
+print("apple","banana", "orange","kiwi",sep="-")'''
+
+# simple data types
+    # int
+# num1 = 10
+# print(num1)
+# print(type(num1))
+# 10 value
+# num1 -> variable
+# assignment operator
+# # float
+# num1 = 10.2
+# print(num1)
+# print(type(num1))
+
+# a=num1
+# print(id(num1))
+# print(id(a))
+# b=10.2
+# print(id(b))
+
+# print(b is num1)
+
+# a=10
+# b=20
+# c=30
+
+# temp = a
+# a=c
+# c=b
+# b = temp
+# print(a,b,c)
+
+# a,b,c = 10,20,30
+# a,b,c = b,c,a
+# print(a,b,c)
+# string
+text = "Hello welcome"
+# h=0 e=1 l=2 l =3 0=4 ""=5 w=6 e=7 l=8 c=9 o=9 m=10 e=11 or -1
+
+
+# print(type(text))
+# print(text)
+print(text[0:6:2])
+print(text[-1::-1])
+print(text[::-1])
+# text[start:end:increment]
+# boolean
+todayClass = True
+
