23c13b008699f69f8e248475a32c3b6575fd5685
Anandhbasha <96278666+Anandhbasha@users.noreply.github.com>
2026-03-23 20:21:23 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,5 +1,6 @@
 newlist = ["101ME101","101ME102","101ME103","101ME104","101ME105"]
 print(newlist[0])
 print(newlist[1])
+# replace new value to the exact index
 newlist[2] = "111ME103"
 print(len(newlist))
