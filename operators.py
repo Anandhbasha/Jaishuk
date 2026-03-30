37ec364dd36b6ca5a27807f3bd8028a2265698f0
Anandhbasha <anadhakm@gmail.com>
2026-03-30 21:02:01 +0530

    operator

---
+++ operators.py
@@ -0,0 +1,35 @@
+#arithmetic
+# +,-,*,/,//,%,**
+a=10
+b=20
+print(a+b)
+print(b-a)
+print(a*b)
+print(b/a) #2.0
+print(b//a) #->floor division
+print(a%b) 
+# 10/20 
+print(3/2)
+print(a**b)
+# 2**3 = 2*2*2
+# assignment
+# =,+=,-=,*=,/=,//=,%=,**=
+a=50
+a=a+10
+print(a)
+a+=10
+print(a)
+a-=10
+print(a)
+a*=2
+print(a)
+a//=4
+print(a)
+# logical operators
+# and or not
+print(a<100 and a>29 and a==28) #all condtion must be true
+print(a>100 or a<29 or a==28) #any one is true output will be the true
+# menbership operators
+# ternary operators
+# Bitwise operators
+# comparision 
