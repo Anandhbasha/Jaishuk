145dad908d3d1896ffe0d6762f80378649c6fdbf
Anandhbasha <anadhakm@gmail.com>
2026-04-06 21:42:57 +0530

    if

---
+++ conditional.py
@@ -0,0 +1,16 @@
+# if
+age=int(input("Enter the Age:"))
+# if(age>19):
+#     print("Adult")
+# if else
+# if(age>19):
+#     print("Adult")
+# else:
+#     print("He is Teenager")
+# elif
+if(age>19):
+    print("Adult")
+elif(age<13):
+    print("Kids")
+else:
+    print("He is Teenager")
--- operators.py
+++ operators.py
@@ -1,44 +1,48 @@
-#arithmetic
-# # +,-,*,/,//,%,**
-# a=10
-# b=20
-# print(a+b)
-# print(b-a)
-# print(a*b)
-# print(b/a) #2.0
-# print(b//a) #->floor division
-# print(a%b) 
-# # 10/20 
-# print(3/2)
-# print(a**b)
-# # 2**3 = 2*2*2
-# # assignment
-# # =,+=,-=,*=,/=,//=,%=,**=
-# a=50
-# a=a+10
-# print(a)
-# a+=10
-# print(a)
-# a-=10
-# print(a)
-# a*=2
-# print(a)
-# a//=4
-# print(a)
-# # logical operators
-# # and or not
-# print(a<100 and a>29 and a==28) #all condtion must be true
-# print(a>100 or a<29 or a==28) #any one is true output will be the true
-# print(100!=10)
-# menbership operators
-# newlist = ["apple","banana","cherry","kiwi"]
-# print("apple" in newlist)
-# print("apple" not in newlist)
-# name = "jai"
-# print("s" not in name)
-# ternary operators
-age = 18
-status = "Eligible" if age>=18 else  "Not Eligible"
-print(status)
-# Bitwise operators
-# comparision 
+# #arithmetic
+# # # +,-,*,/,//,%,**
+# # a=10
+# # b=20
+# # print(a+b)
+# # print(b-a)
+# # print(a*b)
+# # print(b/a) #2.0
+# # print(b//a) #->floor division
+# # print(a%b) 
+# # # 10/20 
+# # print(3/2)
+# # print(a**b)
+# # # 2**3 = 2*2*2
+# # # assignment
+# # # =,+=,-=,*=,/=,//=,%=,**=
+# # a=50
+# # a=a+10
+# # print(a)
+# # a+=10
+# # print(a)
+# # a-=10
+# # print(a)
+# # a*=2
+# # print(a)
+# # a//=4
+# # print(a)
+# # # logical operators
+# # # and or not
+# # print(a<100 and a>29 and a==28) #all condtion must be true
+# # print(a>100 or a<29 or a==28) #any one is true output will be the true
+# # print(100!=10)
+# # menbership operators
+# # newlist = ["apple","banana","cherry","kiwi"]
+# # print("apple" in newlist)
+# # print("apple" not in newlist)
+# # name = "jai"
+# # print("s" not in name)
+# # ternary operators
+# age = 18
+# status = "Eligible" if age>=18 else  "Not Eligible"
+# print(status)
+# # comparision
+# # >,<,<=,>=,==
+# print(10>=10)
+# print(20>=10)
+# print(2>=10)
+# print(10==20)
