76d2690167aaad465e2627ba0235ad66e95f59eb
Anandhbasha <anadhakm@gmail.com>
2026-04-14 21:02:23 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,59 @@
+# a=1 
+# #starting point
+# while a<11:
+#     print(a)
+#     a+=1
+#     # increment
+
+a=10
+# while a>=0:
+#     print(a)
+#     # a-=1
+#     a=a-1
+
+
+# # for loop
+# for i in range(0,10):
+#     print(i)
+
+# a=20
+# for i in range(0,a,2):
+#     print(i)
+
+# newarr = [10,20,30,40,50]
+
+# for i in range(0,len(newarr)):
+#     print(newarr[i])
+
+for i in range(1,5):
+    print(i*"*")
+
+#   *
+#  ***
+# # *****
+# #*******
+# n=int(input("Enter the number rows"))
+# for i in range(n):
+#     print(" "*(n-i-1)+ "*"*(2*i+1))
+
+# list1 = [100,152,147,485,758]
+
+# total = 0
+# for i in range(0,len(list1)):
+#     total+=list1[i]
+# print(total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
