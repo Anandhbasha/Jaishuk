21e2088d5636760ef8f31f64f69d3ec0c2c860fb
Anandhbasha <anadhakm@gmail.com>
2026-04-25 20:30:21 +0530

    kjhh

---
+++ functions.py
@@ -0,0 +1,14 @@
+def calculate_total_marks(tamil, english, maths):
+    total = tamil + english + maths
+    return total
+
+print(calculate_total_marks(85, 90, 95))
+print(calculate_total_marks(82, 91, 60))
+print(calculate_total_marks(80, 70, 50))
+print(calculate_total_marks(85, 90, 50))
+print(calculate_total_marks(85, 90, 50))
+print(calculate_total_marks(85, 90, 50))
+print(calculate_total_marks(85, 90, 50))
+print(calculate_total_marks(85, 90, 50))
+print(calculate_total_marks(85, 90, 50))
+print(calculate_total_marks(85, 90, 50))
---
+++ oops.py
@@ -0,0 +1,35 @@
+# car1Mailage = 25
+# car1Airbags = 5
+# car1Color = "Red"
+# car1NoOfDoors = 4
+# car1NoofWheels = 4
+
+# def acc():
+#     print("Car is accelerating")
+# def brake():
+#     print("Car is braking")
+
+class Car:
+    carMailage;
+    carAirbags ;
+    carColor ;
+    carNoOfDoors = 4
+    carNoofWheels = 4
+    def acc(self):
+        print("Car is accelerating")
+    def brake(self):
+        print("Car is braking")
+car1 = Car()
+car1.carMailage = 25
+car1.carAirbags = 5
+car1.carColor = "Red"
+print(car1.carMailage)
+print(car1.carAirbags)
+car1.acc()
+car2 = Car()
+car2.carMailage = 30
+car2.carAirbags = 6
+car2.carColor = "Blue"
+print(car2.carMailage)
+print(car2.carAirbags)
+car2.brake()
