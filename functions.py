f5da697fdb5f7260704839841d538bb0198a0087
Anandhbasha <anadhakm@gmail.com>
2026-05-02 14:01:46 +0530

    oops

--- functions.py
+++ functions.py
@@ -1,3 +1,33 @@
+# def greet():
+#     print("Hello da!")
+
+# greet()
+
+# def = define (create function)
+# function_name = function  name
+# () = input kuduka use pannuvom
+
+# def greet(name):
+#     print("Hello", name)
+
+# greet("Anand")
+
+# def add(a, b):
+#     return a + b
+
+# result = add(5, 3)
+# print(result)
+
+
+# def ATM_withdraw(balance, amount):
+#     if amount <= balance:
+#         return balance - amount
+#     else:
+#         return "Insufficient balance"
+
+# print(ATM_withdraw(1000, 200))
+
+
 def calculate_total_marks(tamil, english, maths):
     total = tamil + english + maths
     return total
